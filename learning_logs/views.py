066aeb89d226f35780b5e1ac46b01b0ab57137a4
Sungurov <safasgasc.asfg@gmail.com>
2023-06-22 15:21:37 +0300

    add template topics.html and change base.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     """Home page of application learning log"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Page wits topics"""
+    topics = Topic.objects.all
+    context = {"topics": topics}
+    return render(request, 'learning_logs/topics.html', context)
