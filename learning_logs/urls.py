03f58f52583af6633d80c7bcbbee9e085981a58f
Sungurov <safasgasc.asfg@gmail.com>
2023-06-22 16:16:19 +0300

    add templates topic.html and change topics.html. Change urls.py and views.py

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page with list of all topics
     path('topics/', views.topics, name='topics'),
+    # Page with detailed information on a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
